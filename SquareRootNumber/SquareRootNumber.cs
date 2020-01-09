1aed70e0173989220c79fa77fadf8053e735cdb5
dana <dana.changwe@szi.gov.zm>
2020-01-09 11:55:37 +0200

    Task: added a SquareRoot object

---
+++ SquareRootNumber/SquareRootNumber.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using operations;
+
+namespace SquareRootNumber
+{
+    public interface ISquareRoot
+    {
+        public dynamic Root(dynamic a);
+    }
+    public class RootNumber : ISquareRoot
+    {
+        public dynamic result;
+        public dynamic Root(dynamic a)
+        {
+            result = SquareRoot.Root(a);
+            return result;
+        }
+    }
+}
