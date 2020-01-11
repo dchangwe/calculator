b505d3e394df0ad9e139d4c0f4b57e0e05d9afa5
dana <dana.changwe@szi.gov.zm>
2020-01-11 22:05:26 +0200

    Task: added Helpers

---
+++ Helpers/Arrays.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Helpers
+{
+    public class Arrays
+    {
+        public static int Length(int[] array)
+        {
+            return array.Length;
+        }
+        public static int Length(double[] array)
+        {
+            return array.Length; 
+        }
+    }
+}
