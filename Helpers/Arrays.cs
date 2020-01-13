1958ff1c60b79222caf0544b865d5d2b117ff4ee
dana <dana.changwe@szi.gov.zm>
2020-01-13 10:14:00 +0200

    Task: created  Helpers to remove duplication and do rounding off

---
+++ Helpers/Arrays.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Helpers
+{
+    public class Arrays
+    {
+        public static int Length(dynamic array)
+        {
+            return array.Length;
+        }
+    }
+}
