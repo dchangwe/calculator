e14dac462ad7f3a052eb0595d87c751b2e6b9719
dana <dana.changwe@szi.gov.zm>
2020-01-13 10:02:42 +0200

    Task: created a ZScore object

---
+++ StatsZScore/IZScore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatisticsZScore
+{
+  public interface  IZScore
+    {
+        public double StatZScore(dynamic score, dynamic values);
+
+    }
+}
