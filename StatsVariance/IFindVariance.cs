42e1803fd5308cd02e445af9ff1884433d0e1311
dana <dana.changwe@szi.gov.zm>
2020-01-13 10:00:11 +0200

    Task: created a Statistics Variance

---
+++ StatsVariance/IFindVariance.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatisticsVariance
+{
+    public interface  IFindVariance
+    {
+        public double Variance(dynamic arrayA);
+
+    }
+}
